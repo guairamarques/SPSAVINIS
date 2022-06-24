da743d23dc1f703fb656a94c80d56ccb64d45d9d
guaira <guairamarques@yahoo.com.br>
2022-06-23 23:45:12 -0300

    CADPEÇAS

---
+++ CADPECAS.js
@@ -0,0 +1,7 @@
+let peso = 50;
+let listaqntdepecas = 9;
+let nomedapeca = "parafuzeta"
+
+if(peso >= 100){console.log("Peso da peça compatível, pode cadastrar!")}
+if(listaqntdepecas >= 10){console.log(Não há capacidade na lista, caixa cheia!)}
+if(nomedapeca.length < 3){console.log(Nome da peça inválido, precisa conter mais que 3 caracteres!)}
