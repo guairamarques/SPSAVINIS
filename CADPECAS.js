5ad7238d4d5e270139201112aa540377ad1d13c7
guaira <guairamarques@yahoo.com.br>
2022-06-24 00:14:55 -0300

    finalização do trabalho

--- CADPECAS.js
+++ CADPECAS.js
@@ -1,7 +1,9 @@
 let peso = 50;
-let listaqntdepecas = 9;
-let nomedapeca = "parafuzeta"
+let listaqntdepecas = 20;
+let nomedapeca = "pc"
 
-if(peso >= 100){console.log("Peso da peça compatível, pode cadastrar!")}
-if(listaqntdepecas >= 10){console.log(Não há capacidade na lista, caixa cheia!)}
-if(nomedapeca.length < 3){console.log(Nome da peça inválido, precisa conter mais que 3 caracteres!)}
+if(peso >= 100){console.log("Peso da peça compatível, pode cadastrar!")} else{console.log("Peso inferior ao permitido para cadastro")};
+
+if(listaqntdepecas >= 10){console.log("Não há capacidade na lista, caixa cheia!")} 
+
+if(nomedapeca.length < 3){console.log("Nome da peça inválido, precisa conter mais que 3 caracteres!")};
